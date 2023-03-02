ba94f5cf2bb4e0020324641348d8d543dcdcc5af
Jeff Goeken <jgoeken@wgu.edu>
2023-03-02 14:29:21 -0800

    Catching up

---
+++ PySwitcherCTA.py
@@ -0,0 +1,158 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 20 11:15:18 2022
+
+@author: jgoeken
+"""
+
+#mport Modules
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+import os
+
+# Create Root Window
+root = Tk()
+root.title ('PySwitcher')
+root.configure(bg ='white')
+
+photo = tk.PhotoImage(master=root,file=r'C:\Users\jgoeken\OneDrive - MGM Resorts International\Pictures\MGMLogo.png')
+
+image_label = ttk.Label(
+    root,
+    image=photo,
+    background= 'white'
+    
+)
+image_label.pack(side = 'top', fill = X)
+
+#Check for open Patron applications
+Patrontest = bool(os.popen('TASKLIST |find  "Patron.exe"').read())
+print(Patrontest)
+if Patrontest == True:
+    message = """
+    Another instance of Patron detected!
+    Please close all open Patron Management
+    applications before selecting a 
+    property.
+    """
+    messagebox.showwarning(title="Patron Instance Detected",message=message)
+
+
+
+def GetDataSource():
+
+    DataSource = v.get()
+    DBConnectInfo = """
+     <?xml version="1.0" encoding="utf-8"?>
+        <configuration>
+          <system.runtime.remoting>
+             <application>
+                <client>
+                   <wellknown type="Igt.Egs.Data.EGSConnInfoStore, DBConnInfoStore" url="http://{}:8989/EGSConnInfoStore.rem"></wellknown>
+                </client>
+             </application>
+          </system.runtime.remoting>
+          <startup>
+             <supportedRuntime version="v4.0" sku=".NETFramework,Version=v4.8"></supportedRuntime>
+          </startup>
+       </configuration>
+    """
+    print(DBConnectInfo.format(DataSource))
+
+    DBConnectFile = open ("C:\Program Files (x86)\Common Files\IGT Systems\DbConnInfoManager.dll.config","w")
+    DBConnectFile.write(DBConnectInfo.format(DataSource))
+    DBConnectFile.close()
+    if app.get() == 4:
+        os.startfile('C:\\Program Files (x86)\\IGT Systems\\Patron management\\Patron.exe') 
+    elif app.get() == 1:
+        os.startfile('C:\\Program Files (x86)\\IGT Systems\\CTA\\Guardian.exe')  
+    elif app.get() == 2:
+        os.startfile('C:\\Program Files (x86)\\IGT Systems\\CTA\\GTouch.exe') 
+    elif app.get() == 3:
+        os.startfile('C:\\Program Files (x86)\\IGT Systems\\CTA\\TbleDrop.exe') 
+    root.destroy()
+        
+
+# Create Frame inside Root Window
+pane = Frame(root)
+pane.title = 'PySwitcher'
+pane2 = Frame(root)
+pane2.title = 'Product'
+
+#geometry Method
+pane2.pack(fill = BOTH, expand = True,padx=10,pady=5)
+pane.pack(fill = BOTH, expand = True,padx=10,pady=5)
+
+
+# Tkinter string variable
+# able to store any string value
+v = StringVar(pane, "1")
+app = IntVar(pane2, 4)
+# Dictionary to create multiple buttons
+values = {"Aria" : "V00WAARACE11P",
+        "Beau Rivage" : "V21WABRACE11P",
+        "Bellagio" : "V00WABGACE11P",
+        "Borgata" : "V41WABGTAACE11P",
+        #"Cosmopolitan":"TBD",
+        "Detroit" : "V31WADTACE11P",
+        "Empire" : "V43WAACE11P",
+        "Excalibur":"V00WAEXACE11P",
+        "Gold Strike" : "V38WAGTACE11P",
+        "Luxor" : "V00WALXACE11P",
+        "Mandalay Bay":"V00WAMBACE11P",        
+        "MGM Las Vegas" : "V00WAMMACE11P",
+        "Mirage" : "V00WAMRACE11P",
+        "National Harbor" : "V33WAACE11P",
+        "New York, NY" : "V00WANYACE11P",
+        "Northfield Park" : "V44WAACE11P",
+        "Park MGM" : "V00WAMCACE11P",
+        "Springfield" : "V34WAACE11P",
+        "V'Dara" : "V00WAVDACE11P",
+        "Pre-Prod" : "VMBGACE01R",
+        "S7 Pre-Prod" : "V36WAMRACE11R"
+        }
+
+#Build Form
+
+#Label(pane,text = "PySwitcher",
+      #background = "cyan",
+      #font="Arial,20").grid(row=1,columnspan=3,sticky = 'ew')
+
+# Loop is used to create multiple Radiobuttons
+r=1
+c=0
+for (text, value) in values.items():
+    Radiobutton(pane, text = text, variable = v,
+        value = value, indicatoron= 0, 
+        background= "light blue",
+        font="Arial,14",
+        command=GetDataSource).grid(row = r, column=c,sticky='ew', padx=2,pady=2)
+    if c ==2:
+        c=0
+        r +=1
+    else:
+        c += 1
+    rdobtnCTA = ttk.Radiobutton(pane2, text = "CTA", variable = app,value = 1)
+    rdobtnCTAtch = ttk.Radiobutton(pane2, text = "CTA Touch", variable = app,value = 2)
+    rdobtnCTAdrp = ttk.Radiobutton(pane2, text = "CTA Drop", variable = app,value = 3)
+    rdobtnCTAptrn = ttk.Radiobutton(pane2, text = "Patron Management", variable = app,value = 4)
+
+    rdobtnCTA.grid(row = 0, column=0,sticky='ew', padx=5,pady=2)
+    rdobtnCTAtch.grid(row = 0, column=1,sticky='ew', padx=5,pady=2)
+    rdobtnCTAdrp.grid(row = 0, column=2,sticky='ew', padx=5,pady=2)
+    rdobtnCTAptrn.grid(row = 0, column=3,sticky='ew', padx=5,pady=2)
+# Close event loop
+
+try:
+    from ctypes import windll
+
+    windll.shcore.SetProcessDpiAwareness(1)
+finally:
+    root.mainloop()
+   
+#root.mainloop()
+
+print(values.items())
---
+++ Reports/Report-Bonus_Adjustment_By_Adjustor GUI_oop.py
@@ -0,0 +1,70 @@
+import pyodbc
+import pandas as pd
+import openpyxl
+from tkinter import *
+from tkinter import ttk
+from tkcalendar import DateEntry
+
+class Report:
+    def __init__(self):
+        self.__conn = pyodbc.connect('Driver={SQL Server};'
+                            'Server=playdb01r;'
+                            'Database=PlayerManagement;'
+                            'Trusted_Connection=yes;')
+        self.__SiteQuery = 'SELECT Description,SiteID from Site (Nolock)'
+        ReportQuery ="select name from sys.procedures where name like 'REPORT_%' order by name"
+        Sites = pd.read_sql_query(self.__SiteQuery, self.__conn).reset_index(drop=True)
+        Reports = pd.read_sql_query(ReportQuery, self.__conn)
+        print(Reports)
+
+        dataTable = ({
+            'StartDate':[],
+            'EndDate':[],
+            'siteID':[],
+            'Property':[]
+            })
+        df = pd.DataFrame(dataTable)
+
+        def buildStartDateText(lblStartDatetxt,strStartDate):
+            lblStartDatetxt += strStartDate
+            return lblStartDatetxt
+
+        def AssignVariable():
+            print(strStartDate.get())
+            print(strEndDate.get())
+            print(strSiteID.get())
+            print(strReportName.get())
+            lblStartDatetxt = strStartDate.get()
+            
+            lblStartDate.set(lblStartDatetxt)
+
+   
+
+root = Tk()
+
+lblEndDatetxt = ''
+lblSitetxt = ''
+lblReporttxt = ''
+strStartDate = StringVar(root)
+strEndDate = StringVar(root)
+strSiteID = StringVar(root)
+strReportName = StringVar(root)
+root.config(bg='lightblue')
+
+lblStartDatetxt = ''
+
+
+ttk.Label(root,text = 'Start Date',background='lightblue').grid(row=0,column=0,sticky='ew',padx=10,pady=5)
+ttk.Label(root,text = 'End Date Date',background='lightblue').grid(row=0,column=2,sticky='ew',padx=10,pady=5)
+ttk.Label(root,text = 'Site',background='lightblue').grid(row=0,column=4,sticky='ew',padx=10,pady=5)
+ttk.Label(root,text = 'Report',background='lightblue').grid(row=0,column=6,sticky='ew',padx=10,pady=5)
+dpStartDate = DateEntry(root,textvariable=strStartDate).grid(row=0,column = 1,padx=10,pady=5)
+dpEndDate = DateEntry(root,textvariable=strEndDate).grid(row=0,column = 3,padx=10,pady=5)
+ttk.Combobox(root,width=20,textvariable=strSiteID,values=Sites.loc[:,'Description'].to_list()).grid(row=0,column = 5,padx=10,pady=5,sticky='w')
+ttk.Combobox(root,width=40,height=20,textvariable=strReportName,values=Reports.loc[:,'name'].to_list()).grid(row=0,column = 7,padx=10,pady=5,sticky='w')
+btnGO = ttk.Button(root,text = "Request",command = AssignVariable).grid(row=0,column=9,padx=10,pady=10)
+
+lblStartDate = ttk.Label(root,text = lblStartDatetxt,background='lightblue').grid(row=1,column=1,sticky='w',padx=10,pady=5,columnspan=9)
+
+
+root.mainloop()
